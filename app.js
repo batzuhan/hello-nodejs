72080f95d3c7f77c2cf9263e46e7e638f5e8a396
batzuhan <batuhan.ozcan@ozu.edu.tr>
2020-06-17 10:11:54 +0300

    Testing

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello World!\n");
+var express = require('express');
+app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hellooo World!\n');
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
+
